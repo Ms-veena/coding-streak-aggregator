42eac274ee45d8766d51b6283539075be240ee36
Ms-veena <varsabtech@gamil.com>
2026-01-23 10:49:21 +0530

    fix: enable CORS for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,8 @@ console.log("fetchCodeforcesData:", fetchCodeforcesData);
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
+
 app.use(express.json());
 
 // test route
